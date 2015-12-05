b9e6e19bbd1a1bedb927f4a2aee95f044dd4e33e
Ziyad Al Obaidi <ziyadalobaidi@eflexsystems.com>
2015-12-05 17:27:13 -0500

    get updates back into order

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,12 +4,12 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
-    //stylusOptions: {
+    stylusOptions: {
       //includePaths: [
         //'bower_components/bootstap/dist/css',
         //'node_modules/ember-paper/app/styles'
       //]
-    //}
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated

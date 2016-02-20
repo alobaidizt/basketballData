0eebafb07266f862ab729b561c8771328510c0ad
Ziyad Al-Obaidi <alobaidizt@gmail.com>
2016-02-20 14:06:03 -0500

    update gitignore and git /config endpoints working

--- webApp/routes/api.js
+++ webApp/routes/api.js
@@ -3,46 +3,46 @@ var router = express.Router();
 
 var keywords  = require('./api/keyword');
 var histories = require('./api/history');
-var configs = require('./api/config');
+var config = require('./api/config');
 
 /* Keywords routes */
-router.route('/keywords')  
+router.route('/keywords')
     .post(function(req,res) { keywords.addKeyword(req,res); })
     .get(function(req,res) { keywords.getAllKeywords(req,res); });
 
 /* Single keyword routes */
-router.route('/keywords/:keyword_id')  
+router.route('/keywords/:keyword_id')
     .get(function(req, res) { keywords.getSingleKeyword(req, res, req.params.keyword_id); })
     .put(function(req, res) { keywords.updateKeyword(req, res, req.params.keyword_id); })
     .delete(function(req, res) { keywords.deleteKeyword(req, res, req.params.keyword_id); });
 
-router.route('/keywords/name/:keyword_name')  
+router.route('/keywords/name/:keyword_name')
     .get(function(req, res) { keywords.getKeywordByName(req, res, req.params.keyword_name); })
     .put(function(req, res) { keywords.updateKeywordByName(req, res, req.params.keyword_name); })
     .delete(function(req, res) { keywords.deleteKeywordByName(req, res, req.params.keyword_name); });
 
 /* History routes */
-router.route('/histories')  
+router.route('/histories')
     .post(function(req,res) { histories.addRecord(req,res); })
     .get(function(req,res) { histories.getHistoryRecords(req,res); });
 
-router.route('/histories/:session_id')  
+router.route('/histories/:session_id')
     .get(function(req, res) { histories.getHistoryBySession(req, res, req.params.session_id); })
     .delete(function(req, res) { histories.deleteSessionHistory(req, res, req.params.session_id); });
 
 /* Config routes */
-router.route('/configs')  
-    .delete(function(req, res) { configs.deleteConfigs(req, res); });
-router.route('/configs/delay')  
-    .get(function(req,res) { configs.getDelay(req,res); })
-    .put(function(req,res) { configs.updateDelay(req,res); });
-router.route('/configs/stitch')  
-    .get(function(req,res) { configs.getStitches(req,res); })
-    .put(function(req,res) { configs.updateStitches(req,res); })
-    .delete(function(req, res) { configs.deleteStitches(req, res); });
-router.route('/configs/action')  
-    .get(function(req,res) { configs.getActions(req,res); })
-    .put(function(req,res) { configs.updateActions(req,res); })
-    .delete(function(req, res) { configs.deleteActions(req, res); });
+router.route('/config')
+    .delete(function(req, res) { config.deleteConfigs(req, res); });
+router.route('/config/delay')
+    .get(function(req,res) { config.getDelay(req,res); })
+    .put(function(req,res) { config.updateDelay(req,res); });
+router.route('/config/stitch')
+    .get(function(req,res) { config.getStitches(req,res); })
+    .put(function(req,res) { config.updateStitches(req,res); })
+    .delete(function(req, res) { config.deleteStitches(req, res); });
+router.route('/config/action')
+    .get(function(req,res) { config.getActions(req,res); })
+    .put(function(req,res) { config.updateActions(req,res); })
+    .delete(function(req, res) { config.deleteActions(req, res); });
 
 module.exports = router;  

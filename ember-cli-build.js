b4bbecb5729fe9412af63894923e293c5cd558de
Ziyad Al Obaidi <ziyadalobaidi@eflexsystems.com>
2016-05-14 18:35:39 -0400

    clean ember-cli-build file, and add bootstrap js functions

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,17 +2,11 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    stylusOptions: {
-      //includePaths: [
-        //'bower_components/bootstap/dist/css',
-        //'node_modules/ember-paper/app/styles'
-      //]
-    }
-  });
+  var app = new EmberApp(defaults, {});
 
   app.import('bower_components/fontawesome/css/font-awesome.css');
   app.import('bower_components/fontawesome/fonts/fontawesome-webfont.ttf');
+  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
   app.import('bower_components/materialize/dist/css/materialize.css');
   app.import('bower_components/materialize/dist/js/materialize.js');

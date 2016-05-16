9533ee62e16c8d82cea73ef4b5f1d39c2099ff01
Ziyad Al Obaidi <ziyadalobaidi@eflexsystems.com>
2016-05-15 20:57:13 -0400

    tryna  get this working...

--- webApp/models/stat.js
+++ webApp/models/stat.js
@@ -1,22 +1,28 @@
 var mongoose = require('mongoose');  
 var Schema = mongoose.Schema;
+var ObjectId = Schema.Types.ObjectId;
+
+var ActionSchema = new Schema({  
+  count:        { type: Number, default: 0 },
+  stamps:       []
+});
 
 var StatSchema = new Schema({  
   sessionName:        String,
   videoPath:          String,
   playerNumber:       String,
   playerTeam:         String,
-  twoPointAttempt:    {},
-  twoPointMade:       {},
-  threePointAttempt:  {},
-  threePointMade:     {},
-  freeThrowAttempt:   {},
-  freeThrowMade:      {},
-  assist:             {},
-  foul:               {},
-  rebound:            {},
-  turnover:           {},
-  steal:              {}
+  twoPointAttempt:    ActionSchema,
+  twoPointMade:       ActionSchema,
+  threePointAttempt:  ActionSchema,
+  threePointMade:     ActionSchema,
+  freeThrowAttempt:   ActionSchema,
+  freeThrowMade:      ActionSchema,
+  assist:             ActionSchema,
+  foul:               ActionSchema,
+  rebound:            ActionSchema,
+  turnover:           ActionSchema,
+  steal:              ActionSchema
 });
 
 module.exports = mongoose.model('Stat', StatSchema);  
